57cff64668904c8a0d12d2465110f84c56df7162
TEk <t.kambarov@syberry.com>
2025-02-02 22:50:03 +0600

    add repo annotations

--- check/src/main/java/com/sto/check/repository/BidRepository.java
+++ check/src/main/java/com/sto/check/repository/BidRepository.java
@@ -2,6 +2,8 @@ package com.sto.check.repository;
 
 import com.sto.check.model.entity.BidEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BidRepository extends JpaRepository<BidEntity, Long> {
 }
--- repair/src/main/java/com/sto/repair/repository/BidRepository.java
+++ repair/src/main/java/com/sto/repair/repository/BidRepository.java
@@ -2,6 +2,8 @@ package com.sto.repair.repository;
 
 import com.sto.repair.model.entity.BidEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BidRepository extends JpaRepository<BidEntity, Long> {
 }
